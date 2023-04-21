35aae71d336b25cc9950ebb5bcc0856dbd346de7
Alethea Chan <s10221902@connect.np.edu.sg>
2023-04-22 00:19:15 +0800

    committed prompt

--- Question4.java
+++ Question4.java
@@ -6,13 +6,13 @@ public class Question4
   {
     Scanner in = new Scanner(System.in);
     // Prompt for integer
-    System.out.print("Input integer for base of triangle: ");
+    //System.out.print("Input integer for base of triangle: ");
     int triBase=in.nextInt();
     for (int i=1; triBase>=i; i++)
     {
       for (int j=triBase; j>=i; j--)
       {
-        System.out.print("*");
+        //System.out.print("*");
       }
       System.out.println();
     }
